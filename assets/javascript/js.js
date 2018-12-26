08d669bdc268482ddce11ad2cce4755cc5af0a6b
squall2046 <squall2046@hotmail.com>
2018-12-26 09:57:49 -0500

    new css liabrary materializecss, new animate effects

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -22,37 +22,36 @@ $(document).ready(function () {
       method: "GET"
     }).then(function (response) {
       $("#food-photo").attr("src", response.meals[0].strMealThumb);
-      $("#food-photo").prepend("click image turn to video <br>");
 
       // right side changes:
       $("#right-default").hide();
       $("#food-results").empty();
       $("#food-results").append(searchTerm);
-      $("#food-results").append("<h2>Meal: " + response.meals[0].strMeal + "</h2></br>");
+      $("#food-results").append("<h4>Meal: " + response.meals[0].strMeal + "</h4>");
       $("#food-results").append("</br> Area: " + response.meals[0].strArea);
-      $("#food-results").append("</br> Category: </br> " + response.meals[0].strCategory + "</br>");
-      $("#food-results").append("</br> Instructions: </br> " + response.meals[0].strInstructions);
-      $("#food-results").append("</br> Ingredients:</br>");
-      if (response.meals[0].strIngredient1.length > 0) { $("#food-results").append(response.meals[0].strMeasure1 + response.meals[0].strIngredient1 + "<br>") };
-      if (response.meals[0].strIngredient2.length > 0) { $("#food-results").append(response.meals[0].strMeasure2 + response.meals[0].strIngredient2 + "<br>") };
-      if (response.meals[0].strIngredient3.length > 0) { $("#food-results").append(response.meals[0].strMeasure3 + response.meals[0].strIngredient3 + "<br>") };
-      if (response.meals[0].strIngredient4.length > 0) { $("#food-results").append(response.meals[0].strMeasure4 + response.meals[0].strIngredient4 + "<br>") };
-      if (response.meals[0].strIngredient5.length > 0) { $("#food-results").append(response.meals[0].strMeasure5 + response.meals[0].strIngredient5 + "<br>") };
-      if (response.meals[0].strIngredient6.length > 0) { $("#food-results").append(response.meals[0].strMeasure6 + response.meals[0].strIngredient6 + "<br>") };
-      if (response.meals[0].strIngredient7.length > 0) { $("#food-results").append(response.meals[0].strMeasure7 + response.meals[0].strIngredient7 + "<br>") };
-      if (response.meals[0].strIngredient8.length > 0) { $("#food-results").append(response.meals[0].strMeasure8 + response.meals[0].strIngredient8 + "<br>") };
-      if (response.meals[0].strIngredient9.length > 0) { $("#food-results").append(response.meals[0].strMeasure9 + response.meals[0].strIngredient9 + "<br>") };
-      if (response.meals[0].strIngredient10.length > 0) { $("#food-results").append(response.meals[0].strMeasure10 + response.meals[0].strIngredient10 + "<br>") };
-      if (response.meals[0].strIngredient11.length > 0) { $("#food-results").append(response.meals[0].strMeasure11 + response.meals[0].strIngredient11 + "<br>") };
-      if (response.meals[0].strIngredient12.length > 0) { $("#food-results").append(response.meals[0].strMeasure12 + response.meals[0].strIngredient12 + "<br>") };
-      if (response.meals[0].strIngredient13.length > 0) { $("#food-results").append(response.meals[0].strMeasure13 + response.meals[0].strIngredient13 + "<br>") };
-      if (response.meals[0].strIngredient14.length > 0) { $("#food-results").append(response.meals[0].strMeasure14 + response.meals[0].strIngredient14 + "<br>") };
-      if (response.meals[0].strIngredient15.length > 0) { $("#food-results").append(response.meals[0].strMeasure15 + response.meals[0].strIngredient15 + "<br>") };
-      if (response.meals[0].strIngredient16.length > 0) { $("#food-results").append(response.meals[0].strMeasure16 + response.meals[0].strIngredient16 + "<br>") };
-      if (response.meals[0].strIngredient17.length > 0) { $("#food-results").append(response.meals[0].strMeasure17 + response.meals[0].strIngredient17 + "<br>") };
-      if (response.meals[0].strIngredient18.length > 0) { $("#food-results").append(response.meals[0].strMeasure18 + response.meals[0].strIngredient18 + "<br>") };
-      if (response.meals[0].strIngredient19.length > 0) { $("#food-results").append(response.meals[0].strMeasure19 + response.meals[0].strIngredient19 + "<br>") };
-      if (response.meals[0].strIngredient20.length > 0) { $("#food-results").append(response.meals[0].strMeasure20 + response.meals[0].strIngredient20 + "<br>") };
+      $("#food-results").append("</br> Category: " + response.meals[0].strCategory + "</br>");
+      $("#food-results").append("<br> Instructions: <br> " + response.meals[0].strInstructions);
+      $("#food-results").append("<br><br> Ingredients:<br>");
+      if (response.meals[0].strIngredient1.length > 0) { $("#food-results").append(response.meals[0].strMeasure1 + response.meals[0].strIngredient1 + "; ") };
+      if (response.meals[0].strIngredient2.length > 0) { $("#food-results").append(response.meals[0].strMeasure2 + response.meals[0].strIngredient2 + "; ") };
+      if (response.meals[0].strIngredient3.length > 0) { $("#food-results").append(response.meals[0].strMeasure3 + response.meals[0].strIngredient3 + "; ") };
+      if (response.meals[0].strIngredient4.length > 0) { $("#food-results").append(response.meals[0].strMeasure4 + response.meals[0].strIngredient4 + "; ") };
+      if (response.meals[0].strIngredient5.length > 0) { $("#food-results").append(response.meals[0].strMeasure5 + response.meals[0].strIngredient5 + "; ") };
+      if (response.meals[0].strIngredient6.length > 0) { $("#food-results").append(response.meals[0].strMeasure6 + response.meals[0].strIngredient6 + "; ") };
+      if (response.meals[0].strIngredient7.length > 0) { $("#food-results").append(response.meals[0].strMeasure7 + response.meals[0].strIngredient7 + "; ") };
+      if (response.meals[0].strIngredient8.length > 0) { $("#food-results").append(response.meals[0].strMeasure8 + response.meals[0].strIngredient8 + "; ") };
+      if (response.meals[0].strIngredient9.length > 0) { $("#food-results").append(response.meals[0].strMeasure9 + response.meals[0].strIngredient9 + "; ") };
+      if (response.meals[0].strIngredient10.length > 0) { $("#food-results").append(response.meals[0].strMeasure10 + response.meals[0].strIngredient10 + "; ") };
+      if (response.meals[0].strIngredient11.length > 0) { $("#food-results").append(response.meals[0].strMeasure11 + response.meals[0].strIngredient11 + "; ") };
+      if (response.meals[0].strIngredient12.length > 0) { $("#food-results").append(response.meals[0].strMeasure12 + response.meals[0].strIngredient12 + "; ") };
+      if (response.meals[0].strIngredient13.length > 0) { $("#food-results").append(response.meals[0].strMeasure13 + response.meals[0].strIngredient13 + "; ") };
+      if (response.meals[0].strIngredient14.length > 0) { $("#food-results").append(response.meals[0].strMeasure14 + response.meals[0].strIngredient14 + "; ") };
+      if (response.meals[0].strIngredient15.length > 0) { $("#food-results").append(response.meals[0].strMeasure15 + response.meals[0].strIngredient15 + "; ") };
+      if (response.meals[0].strIngredient16.length > 0) { $("#food-results").append(response.meals[0].strMeasure16 + response.meals[0].strIngredient16 + "; ") };
+      if (response.meals[0].strIngredient17.length > 0) { $("#food-results").append(response.meals[0].strMeasure17 + response.meals[0].strIngredient17 + "; ") };
+      if (response.meals[0].strIngredient18.length > 0) { $("#food-results").append(response.meals[0].strMeasure18 + response.meals[0].strIngredient18 + "; ") };
+      if (response.meals[0].strIngredient19.length > 0) { $("#food-results").append(response.meals[0].strMeasure19 + response.meals[0].strIngredient19 + "; ") };
+      if (response.meals[0].strIngredient20.length > 0) { $("#food-results").append(response.meals[0].strMeasure20 + response.meals[0].strIngredient20 + "; ") };
 
       //jumbotron change:
       $("#drink-video").css("display", "none");
@@ -71,43 +70,50 @@ $(document).ready(function () {
       method: "GET"
     }).then(function (response) {
       $("#food-photo").attr("src", response.meals[0].strMealThumb);
-      $("#food-photo").prepend("click image turn to video <br>");
 
       // right side changes:
       $("#right-default").hide();
       $("#food-results").empty();
       $("#food-results").append(searchTerm);
-      $("#food-results").append("<h2>Meal: " + response.meals[0].strMeal + "</h2></br>");
+      $("#food-results").append("<h4>Meal: " + response.meals[0].strMeal + "</h4>");
       $("#food-results").append("</br> Area: " + response.meals[0].strArea);
-      $("#food-results").append("</br> Category: </br> " + response.meals[0].strCategory + "</br>");
-      $("#food-results").append("</br> Instructions: </br> " + response.meals[0].strInstructions);
-      $("#food-results").append("</br> Ingredients:</br>");
-      if (response.meals[0].strIngredient1.length > 0) { $("#food-results").append(response.meals[0].strMeasure1 + response.meals[0].strIngredient1 + "<br>") };
-      if (response.meals[0].strIngredient2.length > 0) { $("#food-results").append(response.meals[0].strMeasure2 + response.meals[0].strIngredient2 + "<br>") };
-      if (response.meals[0].strIngredient3.length > 0) { $("#food-results").append(response.meals[0].strMeasure3 + response.meals[0].strIngredient3 + "<br>") };
-      if (response.meals[0].strIngredient4.length > 0) { $("#food-results").append(response.meals[0].strMeasure4 + response.meals[0].strIngredient4 + "<br>") };
-      if (response.meals[0].strIngredient5.length > 0) { $("#food-results").append(response.meals[0].strMeasure5 + response.meals[0].strIngredient5 + "<br>") };
-      if (response.meals[0].strIngredient6.length > 0) { $("#food-results").append(response.meals[0].strMeasure6 + response.meals[0].strIngredient6 + "<br>") };
-      if (response.meals[0].strIngredient7.length > 0) { $("#food-results").append(response.meals[0].strMeasure7 + response.meals[0].strIngredient7 + "<br>") };
-      if (response.meals[0].strIngredient8.length > 0) { $("#food-results").append(response.meals[0].strMeasure8 + response.meals[0].strIngredient8 + "<br>") };
-      if (response.meals[0].strIngredient9.length > 0) { $("#food-results").append(response.meals[0].strMeasure9 + response.meals[0].strIngredient9 + "<br>") };
-      if (response.meals[0].strIngredient10.length > 0) { $("#food-results").append(response.meals[0].strMeasure10 + response.meals[0].strIngredient10 + "<br>") };
-      if (response.meals[0].strIngredient11.length > 0) { $("#food-results").append(response.meals[0].strMeasure11 + response.meals[0].strIngredient11 + "<br>") };
-      if (response.meals[0].strIngredient12.length > 0) { $("#food-results").append(response.meals[0].strMeasure12 + response.meals[0].strIngredient12 + "<br>") };
-      if (response.meals[0].strIngredient13.length > 0) { $("#food-results").append(response.meals[0].strMeasure13 + response.meals[0].strIngredient13 + "<br>") };
-      if (response.meals[0].strIngredient14.length > 0) { $("#food-results").append(response.meals[0].strMeasure14 + response.meals[0].strIngredient14 + "<br>") };
-      if (response.meals[0].strIngredient15.length > 0) { $("#food-results").append(response.meals[0].strMeasure15 + response.meals[0].strIngredient15 + "<br>") };
-      if (response.meals[0].strIngredient16.length > 0) { $("#food-results").append(response.meals[0].strMeasure16 + response.meals[0].strIngredient16 + "<br>") };
-      if (response.meals[0].strIngredient17.length > 0) { $("#food-results").append(response.meals[0].strMeasure17 + response.meals[0].strIngredient17 + "<br>") };
-      if (response.meals[0].strIngredient18.length > 0) { $("#food-results").append(response.meals[0].strMeasure18 + response.meals[0].strIngredient18 + "<br>") };
-      if (response.meals[0].strIngredient19.length > 0) { $("#food-results").append(response.meals[0].strMeasure19 + response.meals[0].strIngredient19 + "<br>") };
-      if (response.meals[0].strIngredient20.length > 0) { $("#food-results").append(response.meals[0].strMeasure20 + response.meals[0].strIngredient20 + "<br>") };
+      $("#food-results").append("</br> Category: " + response.meals[0].strCategory + "</br>");
+      $("#food-results").append("<br> Instructions: <br> " + response.meals[0].strInstructions);
+      $("#food-results").append("<br><br> Ingredients:<br>");
+      if (response.meals[0].strIngredient1.length > 0) { $("#food-results").append(response.meals[0].strMeasure1 + response.meals[0].strIngredient1 + "; ") };
+      if (response.meals[0].strIngredient2.length > 0) { $("#food-results").append(response.meals[0].strMeasure2 + response.meals[0].strIngredient2 + "; ") };
+      if (response.meals[0].strIngredient3.length > 0) { $("#food-results").append(response.meals[0].strMeasure3 + response.meals[0].strIngredient3 + "; ") };
+      if (response.meals[0].strIngredient4.length > 0) { $("#food-results").append(response.meals[0].strMeasure4 + response.meals[0].strIngredient4 + "; ") };
+      if (response.meals[0].strIngredient5.length > 0) { $("#food-results").append(response.meals[0].strMeasure5 + response.meals[0].strIngredient5 + "; ") };
+      if (response.meals[0].strIngredient6.length > 0) { $("#food-results").append(response.meals[0].strMeasure6 + response.meals[0].strIngredient6 + "; ") };
+      if (response.meals[0].strIngredient7.length > 0) { $("#food-results").append(response.meals[0].strMeasure7 + response.meals[0].strIngredient7 + "; ") };
+      if (response.meals[0].strIngredient8.length > 0) { $("#food-results").append(response.meals[0].strMeasure8 + response.meals[0].strIngredient8 + "; ") };
+      if (response.meals[0].strIngredient9.length > 0) { $("#food-results").append(response.meals[0].strMeasure9 + response.meals[0].strIngredient9 + "; ") };
+      if (response.meals[0].strIngredient10.length > 0) { $("#food-results").append(response.meals[0].strMeasure10 + response.meals[0].strIngredient10 + "; ") };
+      if (response.meals[0].strIngredient11.length > 0) { $("#food-results").append(response.meals[0].strMeasure11 + response.meals[0].strIngredient11 + "; ") };
+      if (response.meals[0].strIngredient12.length > 0) { $("#food-results").append(response.meals[0].strMeasure12 + response.meals[0].strIngredient12 + "; ") };
+      if (response.meals[0].strIngredient13.length > 0) { $("#food-results").append(response.meals[0].strMeasure13 + response.meals[0].strIngredient13 + "; ") };
+      if (response.meals[0].strIngredient14.length > 0) { $("#food-results").append(response.meals[0].strMeasure14 + response.meals[0].strIngredient14 + "; ") };
+      if (response.meals[0].strIngredient15.length > 0) { $("#food-results").append(response.meals[0].strMeasure15 + response.meals[0].strIngredient15 + "; ") };
+      if (response.meals[0].strIngredient16.length > 0) { $("#food-results").append(response.meals[0].strMeasure16 + response.meals[0].strIngredient16 + "; ") };
+      if (response.meals[0].strIngredient17.length > 0) { $("#food-results").append(response.meals[0].strMeasure17 + response.meals[0].strIngredient17 + "; ") };
+      if (response.meals[0].strIngredient18.length > 0) { $("#food-results").append(response.meals[0].strMeasure18 + response.meals[0].strIngredient18 + "; ") };
+      if (response.meals[0].strIngredient19.length > 0) { $("#food-results").append(response.meals[0].strMeasure19 + response.meals[0].strIngredient19 + "; ") };
+      if (response.meals[0].strIngredient20.length > 0) { $("#food-results").append(response.meals[0].strMeasure20 + response.meals[0].strIngredient20 + "; ") };
 
       //jumbotron change:
       $("#drink-video").css("display", "none");
       $("#food-video").css("display", "block");
     })
   });
+
+
+  //==============================
+  // Click Food search picture (Left side)
+  //==============================
+  // $("#food-photo").on("click", function () {
+  //   $("#food-photo").attr("src", "")
+  // })
 
   //==============================
   // Click Sample Food (Left side)
@@ -135,12 +141,11 @@ $(document).ready(function () {
       method: "GET"
     }).then(function (response) {
       $("#drink-photo").attr("src", response.drinks[0].strDrinkThumb);
-      $("#drink-photo").prepend("click image turn to video <br>");
 
       // left side changes:
       $("#left-default").hide();
       $("#drink-results").empty();
-      $("#drink-results").append("<h2>Drink: " + response.drinks[0].strDrink + "</h2></br>");
+      $("#drink-results").append("<h4>Drink: " + response.drinks[0].strDrink + "</h4></br>");
       $("#drink-results").append("</br> Glass type: " + response.drinks[0].strGlass);
       $("#drink-results").append("</br> Instructions: </br> " + response.drinks[0].strInstructions + "</br>");
       $("#drink-results").append("</br> Ingredients:</br>");
@@ -178,12 +183,11 @@ $(document).ready(function () {
       method: "GET"
     }).then(function (response) {
       $("#drink-photo").attr("src", response.drinks[0].strDrinkThumb);
-      $("#drink-photo").prepend("click image turn to video <br>");
 
       // left side changes:
       $("#left-default").hide();
       $("#drink-results").empty();
-      $("#drink-results").append("<h2>Drink: " + response.drinks[0].strDrink + "</h2></br>");
+      $("#drink-results").append("<h4>Drink: " + response.drinks[0].strDrink + "</h4></br>");
       $("#drink-results").append("</br> Glass type: " + response.drinks[0].strGlass);
       $("#drink-results").append("</br> Instructions: </br> " + response.drinks[0].strInstructions + "</br>");
       $("#drink-results").append("</br> Ingredients:</br>");
